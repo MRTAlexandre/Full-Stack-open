51c236af8a78ccd817eb8bb58c0f0924c2879264
Alexandre Teixeira <mrtalexandre@gmail.com>
2025-05-31 00:52:45 -0300

    Refactor App component to use Anecdote component for displaying anecdotes and votes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -3,6 +3,16 @@ import { useState } from 'react'
 const Button = ({text, onClick}) => {
   return (<button onClick={onClick}>{text}</button>)
 }
+
+const Anecdote = ({ title, anecdote, votes}) => {
+  return(
+    <>
+      <h1>{title}</h1>
+      <div>{anecdote}</div>
+      <div>has {votes} votes</div>
+    </>
+  )
+}
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -18,15 +28,22 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0 })
 
+  const highestVotesAnecdoteIndex = Object.keys(votes).reduce((biggestIndex, index) =>
+    votes[index] > votes[biggestIndex] ? index : biggestIndex
+  , 0)
+
+  const hasAnecdotesWithVotes = Object.values(votes).some(vote => vote > 0)
+
   return (
     <div>
-      {anecdotes[selected]}
-      <div>has {votes[selected]} votes</div>
+      <Anecdote title="Anecdote of the day" anecdote={anecdotes[selected]} votes={votes[selected]} />
       <Button text="vote" onClick={() => setVotes({
         ...votes,
         [selected]: votes[selected] + 1
       })}/>
       <Button text="next anecdote" onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}/>
+
+      {hasAnecdotesWithVotes && <Anecdote title="Anecdote with most votes" anecdote={anecdotes[highestVotesAnecdoteIndex]} votes={votes[highestVotesAnecdoteIndex]} />}
     </div>
   )
 }
