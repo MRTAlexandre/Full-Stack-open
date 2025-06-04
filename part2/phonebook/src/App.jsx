b6fec7a6c265019c911351add4a56687d463e547
Alexandre Teixeira <mrtalexandre@gmail.com>
2025-06-03 22:17:48 -0300

    part 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { id: 1, name: 'Arto Hellas', number: '040-123456'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
     if (isNameDuplicated(newName)) {
       return alert(`${newName} is already added to the phonebook`)
     }
-    const nameObject = {name: newName}
+    const nameObject = {id: persons.length + 1, name: newName, number: newNumber}
     setPersons([...persons, nameObject]);
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+    event.preventDefault();
+  }
+
+   const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
     event.preventDefault();
   }
 
@@ -37,12 +44,19 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
         {persons.map(person => {
-          return <div key={person.name}>{person.name}</div>
+          return <div key={person.id}>{person.name} {person.number}</div>
         })}
     </div>
   )
