6464c55cc3780ad5f518abb895b8e64ca4581868
Alexandre Teixeira <mrtalexandre@gmail.com>
2025-06-03 23:06:04 -0300

    part 2.9 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
+import Filter from './components/Filter'
+import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: 'Arto Hellas', number: '040-123456'}
+    { id: 1, name: 'Arto Hellas', number: '040-123456' },
+    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+  const filteredList = (persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())))
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -28,12 +35,19 @@ const App = () => {
     event.preventDefault();
   }
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+    event.preventDefault()
+  }
+
   const isNameDuplicated = (name) => {
     return persons.find(person => person.name === name)
   }
 
   return (
     <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} setFilter={handleFilterChange} />
       <h2>Phonebook</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
@@ -54,10 +68,8 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-        {persons.map(person => {
-          return <div key={person.id}>{person.name} {person.number}</div>
-        })}
+      <h3>Numbers</h3>
+      <Persons persons={filteredList} />
     </div>
   )
 }
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,11 @@
+const Persons = ({ persons }) => {
+  return(
+    <>
+      {persons.map(person => {
+      return <div key={person.id}>{person.name} {person.number}</div>
+      })}
+    </>
+  )
+}
+
+export default Persons;
